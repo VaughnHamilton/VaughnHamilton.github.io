0002147155f904d32e2ced031c5d8cacb83e1827
Vaughn <blandyc@oregonstate.edu>
2024-02-17 19:19:51 +0100

    test1

--- tests.py
+++ tests.py
@@ -1,1 +1,18 @@
 # 362A2TDD
+
+
+import unittest 
+from check_pwd import check_pwd
+
+class TestCase(unittest.TestCase):
+    
+    def test1(self):
+        input = 12345678 
+        expected ='12345678'
+        self.assertFalse(check_pwd(input), expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
