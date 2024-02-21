9bdd8f1901275e6fa1d172114c9d7e7e7744b9e7
Vaughn <blandyc@oregonstate.edu>
2024-02-21 18:36:34 +0100

    passes 8 len password

--- tests.py
+++ tests.py
@@ -1,18 +1,17 @@
 # 362A2TDD
 
 
-import unittest 
+import unittest
 from check_pwd import check_pwd
 
-class TestCase(unittest.TestCase):
-    
-    def test1(self):
-        input = 12345678 
-        expected ='12345678'
-        self.assertFalse(check_pwd(input), expected)
 
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        input = 12345678
+        expected = '12345678'
+        self.assertFalse(check_pwd(input), expected)
 
 
 if __name__ == '__main__':
     unittest.main()
-
