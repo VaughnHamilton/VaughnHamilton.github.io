6cb46f27e14f5fbcd24b1584c6d17343e8cc3f9a
Vaughn <blandyc@oregonstate.edu>
2024-02-21 19:30:33 +0100

    checking for letters

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,9 @@
 
-def check_pwd(password):
-    if len(str(password)) < 8 or len(str(password)) > 20:
+def check_pwd(pword):
+    password = str(pword)
+    if len(password) < 8 or len(password) > 20:
         return False
+    if not password.isalpha():
+        return False
+        
     return True
