5dab7ac0cbd678d68787794b3c29e9a0d7173300
Vaughn <blandyc@oregonstate.edu>
2024-02-21 19:00:40 +0100

    made sure passwords less than 8 and longer than 20 are rejected

--- tests.py
+++ tests.py
@@ -10,12 +10,17 @@ class TestCase(unittest.TestCase):
     def test1(self):
         input = 12345678
         expected = '12345678'
-        self.assertFalse(check_pwd(input), expected)
+        self.assertTrue(check_pwd(input), expected)
 
     def test2(self):
         input = 1234567
         expected = '1234567'
-        self.assertTrue(check_pwd(input), expected)
+        self.assertFalse(check_pwd(input), expected)
+
+    def test3(self):
+        input = 123456789123456789111
+        expected = '123456789123456789111'
+        self.assertFalse(check_pwd(input), expected)
 
 
 if __name__ == '__main__':
