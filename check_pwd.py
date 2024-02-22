da559bdb5b16c430a126880758786bf4b3810bde
Vaughn <blandyc@oregonstate.edu>
2024-02-22 18:24:47 +0100

    edited for numerical

--- check_pwd.py
+++ check_pwd.py
@@ -3,7 +3,7 @@ def check_pwd(pword):
     password = str(pword)
     if len(password) < 8 or len(password) > 20:
         return False
-    if not password.isalpha():
-        return False
+    if any(part.isdigit() for part in password):
+        return True
         
     return True
