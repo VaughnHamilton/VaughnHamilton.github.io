01f10131269325c95a695cf647f33e2c9709702b
Vaughn <blandyc@oregonstate.edu>
2024-02-22 21:11:25 +0100

    now correctly rejects a string containing lower, upper, digit but no symbol

--- check_pwd.py
+++ check_pwd.py
@@ -1,11 +1,14 @@
 
 def check_pwd(pword):
+    symbols = '~`!@#$%^&*()_+-='
     password = str(pword)
     if not any(part.isdigit() for part in password):
         return False
     elif not any(part.islower() for part in password):
         return False
     elif not any(part.isupper() for part in password):
+        return False
+    elif not any(part in symbols for part in password):
         return False
         
     return True
