63047b3cecd1b95fb94f0d5675b98c0177f397d5
Vaughn <blandyc@oregonstate.edu>
2024-02-22 18:35:22 +0100

    made check_pwd return False when no digits present, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -3,7 +3,8 @@ def check_pwd(pword):
     password = str(pword)
     if len(password) < 8 or len(password) > 20:
         return False
-    if any(part.isdigit() for part in password):
-        return True
+    if not any(part.isdigit() for part in password):
+        return False
+    
         
     return True
