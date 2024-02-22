4c8266cca16aae132ae74f7d35ce12491fc20a5a
Vaughn <blandyc@oregonstate.edu>
2024-02-22 18:54:10 +0100

    decided I was going wrong with assignment so changed things around

--- check_pwd.py
+++ check_pwd.py
@@ -1,10 +1,9 @@
 
 def check_pwd(pword):
     password = str(pword)
-    if len(password) < 8 or len(password) > 20:
-        return False
     if not any(part.isdigit() for part in password):
         return False
-    
+    elif not any(part.islower() for part in password):
+        return False
         
     return True
